ea574cc31f61f2ec06b44967cfde36a81116d155
za3a24 <za3a24@yandex.ru>
2013-07-12 13:12:30 +0700

    Added ConfigurationLoader

---
+++ AXNAEngine/AXNAEngine/com/axna/utility/ConfigurationLoader.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace AXNAEngine.com.axna.utility
+{
+    public class ConfigurationLoader
+    {
+        public static T LoadConfiguration<T>(String loadPath) where T : IConfigurationObject, new()
+        {
+            try
+            {
+                var serializer = new XmlSerializer(typeof (T));
+                TextReader reader = new StreamReader(loadPath);
+                T instance = (T)serializer.Deserialize(reader);
+                reader.Close();
+
+                return instance;
+            }
+            catch (IOException e)
+            {
+                MessageBox.Show("Error while loading configuration file! Using default values", "Error",
+                                MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Error);
+                //T instance = default(T);
+                T instance = new T();
+
+                instance.InitDefault();
+                return instance;
+            }
+        }
+
+        public static void SaveConfiguration<T>(T configurationObject, String savePath)
+        {
+            try
+            {
+                var serializer = new XmlSerializer(typeof (T));
+                TextWriter writer = new StreamWriter(savePath);
+                serializer.Serialize(writer, configurationObject);
+                writer.Close();
+            }
+            catch (IOException e)
+            {
+                MessageBox.Show("Error in unloading configuration file!", "Error",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+
+    public interface IConfigurationObject
+    {
+        void InitDefault();
+    }
+}
