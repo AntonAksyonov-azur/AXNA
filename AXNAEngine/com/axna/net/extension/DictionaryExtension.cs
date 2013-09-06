e4ddfe7bca96533b341a1c8ba500269f7b4e3c89
za3a24 <za3a24@yandex.ru>
2013-09-06 18:39:27 +0700

    Added DictionaryExtension: allows to get random item or key from dictionary

---
+++ AXNAEngine/com/axna/net/extension/DictionaryExtension.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AXNAEngine.com.axna.net.extension
+{
+    public static class DictionaryExtension
+    {
+        private static readonly Random Rnd = new System.Random();
+
+        public static TValue GetRandomItem<TKey, TValue>(this Dictionary<TKey, TValue> dictionary)
+        {
+            TValue[] flatten = dictionary.Select(x => x.Value).ToArray();
+            return flatten[Rnd.Next(0, flatten.Length)];
+        }
+
+        //
+        public static TKey GetRandomKey<TKey, TValue>(this Dictionary<TKey, TValue> dictionary)
+        {
+            TKey[] flatten = dictionary.Keys.ToArray();
+            return flatten[Rnd.Next(0, dictionary.Keys.Count)];
+        }
+    }
+}
