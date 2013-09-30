9551917aec9fc4cfdee91bff322e316e89b99f7f
za3a24 <za3a24@yandex.ru>
2013-09-30 12:49:31 +0700

    Added variable OverColor to TextString component making available to change color of selected text

--- AXNAEngine/com/axna/graphics/TextString.cs
+++ AXNAEngine/com/axna/graphics/TextString.cs
@@ -8,16 +8,18 @@ namespace AXNAEngine.com.axna.graphics
     {
         public SpriteFont SpriteFont;
         public String Text;
+        public Color OverColor;
 
-        public TextString(String text, SpriteFont spriteFont)
+        public TextString(String text, SpriteFont spriteFont, Color overColor)
         {
             Text = text;
             SpriteFont = spriteFont;
+            OverColor = overColor;
         }
 
         public override void Render(SpriteBatch spriteBatch, Vector2 position)
         {
-            AXNA.SpriteBatch.DrawString(SpriteFont, Text, GetDrawPosition(position), Color.White);
+            AXNA.SpriteBatch.DrawString(SpriteFont, Text, GetDrawPosition(position), OverColor);
         }
     }
 }
