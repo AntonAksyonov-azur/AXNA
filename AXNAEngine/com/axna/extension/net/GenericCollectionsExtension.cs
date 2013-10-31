fa9cf88ffa7d7d1044258f9c14745cd42b0dd12e
za3a24 <za3a24@yandex.ru>
2013-10-31 13:28:49 +0700

    Added simple LandscapeGenerator

---
+++ AXNAEngine/com/axna/extension/net/GenericCollectionsExtension.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AXNAEngine.com.axna.extension.net
+{
+    public static class GenericCollectionsExtension
+    {
+        private static readonly Random Rnd = new System.Random();
+
+        public static TValue GetRandomItem<TValue>(this TValue[] array)
+        {
+            return array[Rnd.Next(0, array.Length - 1)];
+        }
+
+        #region List
+
+        public static TValue GetRandomItem<TValue>(this List<TValue> list)
+        {
+            return list[Rnd.Next(0, list.Count - 1)];
+        }
+
+        public static TValue RemoveRandomItem<TValue>(this List<TValue> list)
+        {
+            var value = list[Rnd.Next(0, list.Count - 1)];
+            list.Remove(value);
+
+            return value;
+        }
+
+        #endregion
+    }
+}
---
+++ AXNAEngine/com/axna/utility/LandscapeGenerator.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using AXNAEngine.com.axna.extension.net;
+
+namespace AXNAEngine.com.axna.utility
+{
+    public static class LandscapeGenerator
+    {
+        public static int[,] GenerateLandscape(
+            int mapWidth, int mapHeight,
+            List<int> availableLandscapeIds,
+            List<Direction> availableDirections,
+            int seedsCount,
+            int growSteps,
+            int emptyCellId = 0)
+        {
+            // Future result
+            var resultMap = new int[mapHeight, mapWidth];
+
+            // Clear
+            for (int i = 0; i < mapWidth; i++)
+            {
+                for (int j = 0; j < mapHeight; j++)
+                {
+                    resultMap[i, j] = emptyCellId;
+                }
+            }
+
+            // Random
+            var rnd = new Random();
+
+            // Seeds
+            var seeds = new List<Seed>();
+            for (int i = 0; i < seedsCount; i++)
+            {
+                var seed = new Seed(
+                    rnd.Next(0, mapWidth - 1),
+                    rnd.Next(0, mapHeight - 1),
+                    availableLandscapeIds.GetRandomItem());
+                seeds.Add(seed);
+                resultMap[seed.Y, seed.X] = seed.LandscapeValue;
+            }
+
+            // Growing
+            for (int i = 0; i < growSteps; i++)
+            {
+                for (int j = 0; j < seedsCount; j++)
+                {
+                    var direction = availableDirections.GetRandomItem();
+                    var seed = seeds[j];
+                    seed.X += direction.X;
+                    seed.Y += direction.Y;
+
+                    if (seed.X > mapWidth || seed.Y < mapHeight)
+                    {
+                        seed = new Seed(
+                            rnd.Next(0, mapWidth - 1),
+                            rnd.Next(0, mapHeight - 1),
+                            availableLandscapeIds.GetRandomItem());
+                    }
+                    resultMap[seed.Y, seed.X] = seed.LandscapeValue;
+                }
+            }
+
+            return resultMap;
+        }
+
+        public struct Direction
+        {
+            public int X;
+            public int Y;
+        }
+
+        public static List<Direction> GenerateNESW()
+        {
+            var result = new List<Direction>
+            {
+                new Direction {X = 0, Y = 1}, // N
+                new Direction {X = 1, Y = 0}, // E
+                new Direction {X = 0, Y = -1}, // S
+                new Direction {X = -1, Y = 0}, // W
+            };
+
+            return result;
+        }
+
+        public static List<Direction> GenerateRose()
+        {
+            var result = new List<Direction>
+            {
+                new Direction {X = 0, Y = 1}, // N
+                new Direction {X = 1, Y = 1}, // NE
+                new Direction {X = 1, Y = 0}, // E
+                new Direction {X = 1, Y = -1}, // SE
+                new Direction {X = 0, Y = -1}, // S
+                new Direction {X = -1, Y = -1}, // SW
+                new Direction {X = -1, Y = 0}, // W
+                new Direction {X = -1, Y = 1}, // NW
+            };
+
+            return result;
+        }
+
+        private struct Seed
+        {
+            public int X;
+            public int Y;
+            public int LandscapeValue;
+
+            public Seed(int x, int y, int lanscapeValue)
+            {
+                X = x;
+                Y = y;
+                LandscapeValue = lanscapeValue;
+            }
+        }
+    }
+}
